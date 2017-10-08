b6ac0a7d5635e197fa8d2df9db4cd0936d0c55b3
Matheus Costa <matheuscostap@gmail.com>
2017-10-08 01:33:22 -0300

    Module Pattern

--- functions.js
+++ functions.js
@@ -68,6 +68,7 @@ var pessoa3 = {
 	getNome: getNome
 }
 
+console.log("Call e Apply");
 console.log(getNome.call(pessoa3, " Geromel"));
 console.log(getNome.apply(pessoa3, [" Geromel"]));
 
@@ -82,6 +83,7 @@ var criarTime = function(nome,estadio){
 	};
 };
 
+console.log("Função Fábrica");
 console.log(criarTime("Grêmio","Arena do Grêmio"));
 console.log(criarTime("Tottenham","White Hart Lane"));
 
@@ -91,6 +93,97 @@ var Carro = function(nome,marca){
 	this.marca = marca;
 };
 
+console.log("Função Construtora");
 console.log(new Carro("Golf","Volkswagen"));
 console.log(new Carro("Camaro","Chevrolet"));
 
+
+//Encapsulamento com Factory Function e Closures
+var createCounter = function(){ //Factory Function, é apenas a função de um contador que deve ser atribuida para uma variavel
+	var value = 0; //value encapsulado
+	return { //retorna um objeto que contem uma propriedade add que é uma function ***
+		add: function(){
+			return ++value; //sem o this porque está dentro de um objeto diferente, retornaria undefined
+		}
+	};
+};
+
+var counter = createCounter(); //Atribuindo uma função contador para uma variável
+
+console.log("Encapsulamento com Factory Function e Closures");
+console.log(counter.value); //undefined
+console.log(counter.add()); //Adiciona +1
+console.log(counter.add());
+console.log(counter.add());
+
+//Conclusão: a variavel value se tornou privada e a função add se tornou publica
+// *** O mesmo que um objeto diretamente criado -> return {add: function(){return value++;}
+
+//Encapsulamento com Constructor Function
+var Counter = function(){
+	var value = 0; //value encapsulado
+	this.add = function(){ //função publica
+		return ++value;
+	}
+}
+
+var counter2 = new Counter();
+
+console.log("Encapsulamento com Constructor Function");
+console.log(counter2.value); //undefined
+console.log(counter2.add()); //Adiciona +1
+console.log(counter2.add());
+console.log(counter2.add());
+
+
+
+//Função Imediatamente Ivocada
+//Module Pattern
+var contador = (function(){ // var variavel = (function(){})();
+	var value = 0;
+	return{
+		add: function(){
+			console.log("Adicionou!");
+			return ++value;
+		},
+		reset: function(){
+			console.log("Resetou!");
+			value = 0;
+		}
+	}
+})();
+
+console.log("Module Pattern");
+console.log(contador.value); //undefined
+console.log(contador.add()); //Adiciona +1
+console.log(contador.add());
+console.log(contador.add());
+console.log(contador.reset());
+console.log(contador.add());
+
+
+//Revealing Module Pattern
+var contador = (function(){ // var variavel = (function(){})();
+	var value = 0; //encapsulado
+	var add = function(){ //encapsulado
+		console.log("Adicionou!");
+		return ++value;
+	};
+	var	reset = function(){ //encapsulado
+		console.log("Resetou!");
+		value = 0;
+	}
+	return{
+		add: add, //publico
+		reset: reset	//publico
+	}
+})();
+
+console.log("Revealing Module Pattern");
+console.log(contador.value); //undefined
+console.log(contador.add()); //Adiciona +1
+console.log(contador.add());
+console.log(contador.add());
+console.log(contador.reset());
+console.log(contador.add());
+ 
